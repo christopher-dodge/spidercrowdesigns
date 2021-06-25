c8cdf9155cf6b25640a4144e165d3cf5419024cb
Christopher Dodge <dodge.christopher@icloud.com>
2021-06-24 20:16:54 -0400

    Add sliderImage array

---
+++ src/Components/sliderImage.js
@@ -0,0 +1,109 @@
+const SliderImage = [
+  {
+    title: "Pretty flowers",
+    description: "Test Unsplash URL 1",
+    source: "https://images.unsplash.com/photo-1624220719583-b39d3f2b368c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1932&q=80"
+  },
+  {
+    title: "Ferris Wheel",
+    description: "Test Unsplash URL 2",
+    source: "https://images.unsplash.com/photo-1624038434488-e75993217f9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
+  },
+  {
+    title: "Mountains and the Beach",
+    description: "Test Unsplash URL 3",
+    source: "https://images.unsplash.com/photo-1624031031143-7a084c514946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2281&q=80"
+  }
+  // {
+  //   title: "spider-crow-pic-1.JPG",
+  //   description: "Test Image Description 1",
+  //   source: "../../images/spider-crow-pic-1.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-2.JPG",
+  //   description: "Test Image Description 2",
+  //   source: "../../images/spider-crow-pic-2.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-3.JPG",
+  //   description: "Test Image Description 3",
+  //   source: "../../images/spider-crow-pic-3.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-4.JPG",
+  //   description: "Test Image Description 4",
+  //   source: "../../images/spider-crow-pic-4.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-5.JPG",
+  //   description: "Test Image Description 5",
+  //   source: "../../images/spider-crow-pic-5.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-6.JPG",
+  //   description: "Test Image Description 6",
+  //   source: "../../images/spider-crow-pic-6.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-7.JPG",
+  //   description: "Test Image Description 7",
+  //   source: "../../images/spider-crow-pic-7.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-8.JPG",
+  //   description: "Test Image Description 8",
+  //   source: "../../images/spider-crow-pic-8.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-9.JPG",
+  //   description: "Test Image Description 9",
+  //   source: "../../images/spider-crow-pic-9.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-10.JPG",
+  //   description: "Test Image Description 10",
+  //   source: "../../images/spider-crow-pic-10.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-11.JPG",
+  //   description: "Test Image Description 11",
+  //   source: "../../images/spider-crow-pic-11.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-12.JPG",
+  //   description: "Test Image Description 12",
+  //   source: "../../images/spider-crow-pic-12.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-13.JPG",
+  //   description: "Test Image Description 13",
+  //   source: "../../images/spider-crow-pic-13.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-14.JPG",
+  //   description: "Test Image Description 14",
+  //   source: "../../images/spider-crow-pic-14.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-15.JPG",
+  //   description: "Test Image Description 15",
+  //   source: "../../images/spider-crow-pic-15.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-16.JPG",
+  //   description: "Test Image Description 16",
+  //   source: "../../images/spider-crow-pic-16.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-17.JPG",
+  //   description: "Test Image Description 17",
+  //   source: "../../images/spider-crow-pic-17.JPG"
+  // },
+  // {
+  //   title: "spider-crow-pic-18.JPG",
+  //   description: "Test Image Description 18",
+  //   source: "../../images/spider-crow-pic-18.JPG"
+  // }
+];
+
+export default SliderImage;
