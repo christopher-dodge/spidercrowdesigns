2a1d17614e8a59b94376a0a0dbfcb736a78f88bb
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-25 20:48:41 -0400

    Change NavItems

--- src/Components/NavItems.js
+++ src/Components/NavItems.js
@@ -1,19 +1,19 @@
 export const NavItems = [
-  {
-    title: 'Artwork',
-    link: '#artwork',
-    cName: 'nav-links'
-  },
+  // {
+  //   title: 'Artwork',
+  //   link: '#artwork',
+  //   cName: 'nav-links'
+  // },
   {
     title: 'About',
     link: '#about',
     cName: 'nav-links'
   },
-  {
-    title: 'Education',
-    link: '#education',
-    cName: 'nav-links'
-  },
+  // {
+  //   title: 'Education',
+  //   link: '#education',
+  //   cName: 'nav-links'
+  // },
   {
     title: 'Donations',
     link: '#donations',
