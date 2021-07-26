8e349e6eb5b6610979cbb0b668ddf19c89b98440
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-25 20:29:15 -0400

    Add contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,20 +1,21 @@
-import React from 'react'
+import React from 'react';
+import './Contact.css'
 
 export default function Contact() {
   return (
     <section id="contact" className="contact">
-      <h1>Contact</h1>
-      <h2>Feel free to reach out</h2>
-      <h3>Have a question, comment, conern, drop us a line and we'll get back to you as soon as we can.</h3>
-      <form className="contact-form" action="">
-        <div className="form-row justify-content-center">
-          <input type="text" name="name" className="" placeholder="Full Name" />
-        </div>
-        <input name="bot-field" type="email" style={{visibility: "hidden", display: "none"}} />
-        <input type="email" name="email" class="form-control" placeholder="Email" />
-        <textarea name="message" className="form-control" rows="5" placeholder="Message" />
-        <button type="submit" class="btn">Send Message</button>
-      </form>
+      <hr />
+      <div className="contact-form">
+        <h1>Contact</h1>
+        <h2>Have a question, comment, conern, drop us a line and we'll get back to you as soon as we can.</h2>
+        <form className="" action="">
+          <input type="text" name="name" className="" placeholder="Name" required />
+          <input name="bot-field" type="email" style={{visibility: "hidden", display: "none"}} />
+          <input type="email" name="email" placeholder="Email" />
+          <textarea name="message" rows="5" placeholder="Message" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </section>
   )
 }
