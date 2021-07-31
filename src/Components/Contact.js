2a14ff904e3ac3d831d244b0841d7fa1ebebf411
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-31 19:47:51 -0400

    Fix netlify contact form attributes

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
       <div className="contact-form">
         <h1>Contact</h1>
         <h2>Have a question, comment, conern, drop us a line and we'll get back to you as soon as we can.</h2>
-        <form name="contact" netlify>
+        <form name="contact-form" method="POST" data-netlify-recaptcha="true" netlify-honeypot="bot-field" data-netlify="true">
           <input type="text" name="name" placeholder="Name" required />
           <input name="bot-field" type="email" style={{visibility: "hidden", display: "none"}} />
           <input type="email" name="email" placeholder="Email" />
