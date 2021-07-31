7417aa8d6a22d8e8f97354817c06e2b5dc0c9b53
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-31 16:04:49 -0400

    Add initial production images

--- src/Components/sliderImage.js
+++ src/Components/sliderImage.js
@@ -1,123 +1,55 @@
+import pic1 from '../images/crane-earings.JPG'
+import pic2 from '../images/snake-earings.JPG'
+import pic3 from '../images/virgo-earings.JPG'
+
 const SliderImage = [
   {
-    title: "Pretty flowers",
-    description: "Test Unsplash URL 1",
-    source: "https://images.unsplash.com/photo-1624220719583-b39d3f2b368c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1932&q=80"
+    title: "Crane Earings",
+    description: "Test Image Description 1",
+    alt: "Crane Earings",
+    source: pic1
   },
   {
-    title: "Ferris Wheel",
-    description: "Test Unsplash URL 2",
-    source: "https://images.unsplash.com/photo-1624038434488-e75993217f9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
+    title: "Snake Earings",
+    description: "Test Image Description 1",
+    alt: "Snake Earings",
+    source: pic2
   },
   {
-    title: "Mountains and the Beach",
-    description: "Test Unsplash URL 3",
-    source: "https://images.unsplash.com/photo-1624031031143-7a084c514946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2281&q=80"
-  },
-  {
-    title: "Abstract blue and red",
-    description: "Test Unsplash URL 4",
-    source: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-  },
-  {
-    title: "Abstract baby blue and pink",
-    description: "Test Unsplash URL 5",
-    source: "https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
-  },
-  {
-    title: "Space is cray",
-    description: "Test Unsplash URL 6",
-    source: "https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=80"
+    title: "Virgo Earings",
+    description: "Test Image Description 1",
+    alt: "Virgo Earings",
+    source: pic3
   }
   // {
-  //   title: "spider-crow-pic-1.JPG",
-  //   description: "Test Image Description 1",
-  //   source: "../../images/spider-crow-pic-1.JPG"
+  //   title: "Pretty flowers",
+  //   description: "Test Unsplash URL 1",
+  //   source: "https://images.unsplash.com/photo-1624220719583-b39d3f2b368c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1932&q=80"
   // },
   // {
-  //   title: "spider-crow-pic-2.JPG",
-  //   description: "Test Image Description 2",
-  //   source: "../../images/spider-crow-pic-2.JPG"
+  //   title: "Ferris Wheel",
+  //   description: "Test Unsplash URL 2",
+  //   source: "https://images.unsplash.com/photo-1624038434488-e75993217f9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
   // },
   // {
-  //   title: "spider-crow-pic-3.JPG",
-  //   description: "Test Image Description 3",
-  //   source: "../../images/spider-crow-pic-3.JPG"
+  //   title: "Mountains and the Beach",
+  //   description: "Test Unsplash URL 3",
+  //   source: "https://images.unsplash.com/photo-1624031031143-7a084c514946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2281&q=80"
   // },
   // {
-  //   title: "spider-crow-pic-4.JPG",
-  //   description: "Test Image Description 4",
-  //   source: "../../images/spider-crow-pic-4.JPG"
+  //   title: "Abstract blue and red",
+  //   description: "Test Unsplash URL 4",
+  //   source: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
   // },
   // {
-  //   title: "spider-crow-pic-5.JPG",
-  //   description: "Test Image Description 5",
-  //   source: "../../images/spider-crow-pic-5.JPG"
+  //   title: "Abstract baby blue and pink",
+  //   description: "Test Unsplash URL 5",
+  //   source: "https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
   // },
   // {
-  //   title: "spider-crow-pic-6.JPG",
-  //   description: "Test Image Description 6",
-  //   source: "../../images/spider-crow-pic-6.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-7.JPG",
-  //   description: "Test Image Description 7",
-  //   source: "../../images/spider-crow-pic-7.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-8.JPG",
-  //   description: "Test Image Description 8",
-  //   source: "../../images/spider-crow-pic-8.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-9.JPG",
-  //   description: "Test Image Description 9",
-  //   source: "../../images/spider-crow-pic-9.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-10.JPG",
-  //   description: "Test Image Description 10",
-  //   source: "../../images/spider-crow-pic-10.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-11.JPG",
-  //   description: "Test Image Description 11",
-  //   source: "../../images/spider-crow-pic-11.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-12.JPG",
-  //   description: "Test Image Description 12",
-  //   source: "../../images/spider-crow-pic-12.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-13.JPG",
-  //   description: "Test Image Description 13",
-  //   source: "../../images/spider-crow-pic-13.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-14.JPG",
-  //   description: "Test Image Description 14",
-  //   source: "../../images/spider-crow-pic-14.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-15.JPG",
-  //   description: "Test Image Description 15",
-  //   source: "../../images/spider-crow-pic-15.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-16.JPG",
-  //   description: "Test Image Description 16",
-  //   source: "../../images/spider-crow-pic-16.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-17.JPG",
-  //   description: "Test Image Description 17",
-  //   source: "../../images/spider-crow-pic-17.JPG"
-  // },
-  // {
-  //   title: "spider-crow-pic-18.JPG",
-  //   description: "Test Image Description 18",
-  //   source: "../../images/spider-crow-pic-18.JPG"
+  //   title: "Space is cray",
+  //   description: "Test Unsplash URL 6",
+  //   source: "https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=80"
   // }
 ];
 
