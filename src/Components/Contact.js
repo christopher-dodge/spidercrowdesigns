8ae55a4b87d9f7747adb996de3166b001c90b58d
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-31 19:33:45 -0400

    Add netlify to contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,11 +8,12 @@ export default function Contact() {
       <div className="contact-form">
         <h1>Contact</h1>
         <h2>Have a question, comment, conern, drop us a line and we'll get back to you as soon as we can.</h2>
-        <form className="" action="">
-          <input type="text" name="name" className="" placeholder="Name" required />
+        <form name="contact" netlify>
+          <input type="text" name="name" placeholder="Name" required />
           <input name="bot-field" type="email" style={{visibility: "hidden", display: "none"}} />
           <input type="email" name="email" placeholder="Email" />
           <textarea name="message" rows="5" placeholder="Message" />
+          <div data-netlify-recaptcha="true"></div>
           <button type="submit">Submit</button>
         </form>
       </div>
