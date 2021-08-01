b5977669ed6b6c0c853520f92be21dfa68545cf9
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-31 20:34:48 -0400

    Fix contact netlify three

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
       <div className="contact-form">
         <h1>Contact</h1>
         <h2>Have a question, comment, conern, drop us a line and we'll get back to you as soon as we can.</h2>
-        <form name="contact" netlify netlify-honeypot="bot-field">
+        <form name="contact" netlify-honeypot="bot-field" method="POST" data-netlify="true">
           <input type="text" name="name" placeholder="Name" required />
           <input name="bot-field" type="email" style={{visibility: "hidden", display: "none"}} />
           <input type="hidden" name="form-name" value="contact" />
