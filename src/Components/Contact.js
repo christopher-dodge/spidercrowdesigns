d99362e9774b4e31bd839d3c62fbcfd1a3313fe7
Christopher Dodge <dodge.christopher@icloud.com>
2021-07-31 20:43:43 -0400

    Quickfix remove contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -8,14 +8,7 @@ export default function Contact() {
       <div className="contact-form">
         <h1>Contact</h1>
         <h2>Have a question, comment, conern, drop us a line and we'll get back to you as soon as we can.</h2>
-        <form name="contact" netlify-honeypot="bot-field" method="POST" data-netlify="true">
-          <input type="text" name="name" placeholder="Name" required />
-          <input name="bot-field" type="email" style={{visibility: "hidden", display: "none"}} />
-          <input type="hidden" name="form-name" value="contact" />
-          <input type="email" name="email" placeholder="Email" />
-          <textarea name="message" rows="5" placeholder="Message" />
-          <button type="submit">Submit</button>
-        </form>
+        <h3>spidercrowdesigns@gmail.com</h3>
       </div>
     </section>
   )
