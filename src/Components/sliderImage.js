82a6352880dc272f5078a289c336f13e992c70a9
Christopher Dodge <dodge.christopher@icloud.com>
2022-02-19 22:42:44 -0500

    Update slider gallery

--- src/Components/sliderImage.js
+++ src/Components/sliderImage.js
@@ -1,6 +1,16 @@
-import pic1 from '../images/crane-earings.JPG'
-import pic2 from '../images/snake-earings.JPG'
-import pic3 from '../images/virgo-earings.JPG'
+import pic1 from '../images/crane-earrings1.jpeg'
+import pic2 from '../images/snake-earrings1.jpeg'
+import pic3 from '../images/various-earrings1.jpeg'
+import pic4 from '../images/aquarius-earrings1.jpeg'
+import pic5 from '../images/flower-of-life-earrings1.jpeg'
+import pic6 from '../images/moon-earrings1.jpeg'
+import pic7 from '../images/ohm-earrings.jpeg'
+import pic8 from '../images/various-earrings2.jpeg'
+import pic9 from '../images/virgo-earrings.jpeg'
+import pic10 from '../images/crane-earrings2.jpeg'
+import pic11 from '../images/moon-earrings2.jpeg'
+import pic12 from '../images/snake-earrings2.jpeg'
+import pic13 from '../images/snake-earrings3.jpeg'
 
 const SliderImage = [
   {
@@ -16,41 +26,72 @@ const SliderImage = [
     source: pic2
   },
   {
+    title: "various-earrings",
+    description: "Test Image Description 1",
+    alt: "various-earrings",
+    source: pic3
+  },
+  {
+    title: "aquarius-earrings",
+    description: "Test Image Description 1",
+    alt: "aquarius-earrings",
+    source: pic4
+  },
+  {
+    title: "flower-of-life-earrings",
+    description: "Test Image Description 1",
+    alt: "flower-of-life-earrings",
+    source: pic5
+  },
+  {
+    title: "moon-earrings",
+    description: "Test Image Description 1",
+    alt: "moon-earrings",
+    source: pic6
+  },
+  {
+    title: "ohm-earrings",
+    description: "Test Image Description 1",
+    alt: "ohm-earrings",
+    source: pic7
+  },
+  {
+    title: "various-earrings2",
+    description: "Test Image Description 1",
+    alt: "various-earrings2",
+    source: pic8
+  },
+  {
     title: "Virgo Earings",
     description: "Test Image Description 1",
     alt: "Virgo Earings",
-    source: pic3
+    source: pic9
+  },
+  {
+    title: "Crane Earings 2",
+    description: "Test Image Description 1",
+    alt: "Crane Earings 2",
+    source: pic10
+  },
+  {
+    title: "moon-earrings2",
+    description: "Test Image Description 1",
+    alt: "moon-earrings2",
+    source: pic11
+  },
+  {
+    title: "snake-earrings2",
+    description: "Test Image Description 1",
+    alt: "snake-earrings2",
+    source: pic12
+  },
+  {
+    title: "snake-earrings3",
+    description: "Test Image Description 1",
+    alt: "snake-earrings3",
+    source: pic13
   }
-  // {
-  //   title: "Pretty flowers",
-  //   description: "Test Unsplash URL 1",
-  //   source: "https://images.unsplash.com/photo-1624220719583-b39d3f2b368c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1932&q=80"
-  // },
-  // {
-  //   title: "Ferris Wheel",
-  //   description: "Test Unsplash URL 2",
-  //   source: "https://images.unsplash.com/photo-1624038434488-e75993217f9a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2250&q=80"
-  // },
-  // {
-  //   title: "Mountains and the Beach",
-  //   description: "Test Unsplash URL 3",
-  //   source: "https://images.unsplash.com/photo-1624031031143-7a084c514946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2281&q=80"
-  // },
-  // {
-  //   title: "Abstract blue and red",
-  //   description: "Test Unsplash URL 4",
-  //   source: "https://images.unsplash.com/photo-1541701494587-cb58502866ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80"
-  // },
-  // {
-  //   title: "Abstract baby blue and pink",
-  //   description: "Test Unsplash URL 5",
-  //   source: "https://images.unsplash.com/photo-1557672172-298e090bd0f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80"
-  // },
-  // {
-  //   title: "Space is cray",
-  //   description: "Test Unsplash URL 6",
-  //   source: "https://images.unsplash.com/photo-1484589065579-248aad0d8b13?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1296&q=80"
-  // }
+  
 ];
 
 export default SliderImage;
